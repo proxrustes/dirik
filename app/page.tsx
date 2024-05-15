3ea7c0d79cef29c6fb0403e8affe29695887cea4
proxrustes <proxrustes@gmail.com>
2024-05-15 14:22:21 +0200

    + main page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+
+import { Button } from '@mui/material'
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <main>
-      hui
-    </main>
-  );
+    <>
+      <Button type="button" onClick={() => router.push('/login')}>
+        login
+      </Button>
+      <Button type="button" onClick={() => router.push('/register')}>
+        reg
+      </Button>
+
+    </>
+
+  )
 }
