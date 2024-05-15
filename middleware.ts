18c588ca63f7bfd9f60a6b4cd952c9a0aef5a73e
proxrustes <proxrustes@gmail.com>
2024-05-15 14:26:37 +0200

    + middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname === "/") {
+      req.nextUrl.pathname = "/"
+      return NextResponse.redirect(req.nextUrl)
+    }
+    if (req.nextUrl.pathname === "/dashboard") {
+        req.nextUrl.pathname = "/dashboard/current-shift"
+        return NextResponse.redirect(req.nextUrl)
+      }
+      
+    return NextResponse.next()
+  }
