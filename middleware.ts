9d28db9131a29281dd442a9668394290b0ddb4d9
proxrustes <proxrustes@gmail.com>
2024-05-15 14:26:54 +0200

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
     if (req.nextUrl.pathname === "/") {
-      req.nextUrl.pathname = "/"
+      req.nextUrl.pathname = "/login"
       return NextResponse.redirect(req.nextUrl)
     }
     if (req.nextUrl.pathname === "/dashboard") {
